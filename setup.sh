16a7fddb381a7d4f64741af8527e1259e5774ccc
Aditya Oli <adityaoligehu@gmail.com>
2020-12-23 14:31:11 +0530

    Initial Edit

--- setup.sh
+++ setup.sh
@@ -1,2 +1,3 @@
 docker pull jenkins/jenkins:lts
-docker run -d jenkins --image=jenkins -p 8080:8080
+docker run -p 8080:8080 --name=jenkins-master -d --env JAVA_OPTS="-Xmx8192m" jenkins/jenkins
+docker exec $(docker container ls | grep "jenkins/jenkins" | awk {'print $1'}) cat /var/jenkins_home/secrets/initialAdminPassword
