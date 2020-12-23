19a82479bbc8de836f4e5db0ca5f30d311270c37
Aditya Oli <adityaoligehu@gmail.com>
2020-12-23 12:44:54 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,2 @@
-docker network create jenkins
-docker run --name jenkins-docker --rm --detach --privileged --network jenkins --network-alias docker --env DOCKER_TLS_CERTDIR=/certs \
-  --volume jenkins-docker-certs:/certs/client --volume jenkins-data:/var/jenkins_home --publish 2376:2376 docker:dind
-  
+docker pull jenkins/jenkins:lts
+docker run -d jenkins --image=jenkins -p 8080:8080
